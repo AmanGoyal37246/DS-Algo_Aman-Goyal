4417bfbdf42c412eb8ecb6d91780e077a985f1ee
AmanGoyal37246 <53435873+AmanGoyal37246@users.noreply.github.com>
2020-07-08 19:32:07 +0530

    Update Minimum Coins.cpp

--- Programming Questions/Day 7/Minimum Coins.cpp
+++ Programming Questions/Day 7/Minimum Coins.cpp
@@ -1,4 +1,4 @@
-You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+/*You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
 
 INPUT FORMAT
 
@@ -32,7 +32,7 @@ SAMPLE INPUT
 3 11
 1 3 5
 SAMPLE OUTPUT 
-3
+3*/
 
 
 #include<bits/stdc++.h>
